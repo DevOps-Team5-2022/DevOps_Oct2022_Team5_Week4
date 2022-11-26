866fdd13b77d8fa9071c1e89aca43cc560b63206
TJJ101 <S10194152@connect.np.edu.sg>
2022-11-26 15:37:34 +0800

    Updated helloworld.py file

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,13 @@
-def helloworld():
-    print("Hello World!")
+def helloworld(input):
+    #check if input is empty
+    if not input.strip():
+        print("Input string can't be empty!");
+
+    #check if input is longer than 10 char
+    elif len(input) > 10:
+        print("Input string too long!");
+        
+    else:
+        print("Hello " + input + "! Welcome to Hello World File!");
  
-helloworld()
+helloworld("Team");
